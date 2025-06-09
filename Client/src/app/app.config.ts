0680dc39b8c9c2bb8d3c616f2e5876eca003ccfc
limited-ed <yarosroman@gmail.com>
2025-06-09 23:05:56 +0800

    dev

--- Client/src/app/app.config.ts
+++ Client/src/app/app.config.ts
@@ -1,11 +1,11 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, inject, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
-import { provideStore } from '@ngxs/store';
-import { withNgxsReduxDevtoolsPlugin  } from '@ngxs/devtools-plugin';
+import { provideStore, Store } from '@ngxs/store';
+import { withNgxsReduxDevtoolsPlugin } from '@ngxs/devtools-plugin';
 import { withNgxsStoragePlugin } from '@ngxs/storage-plugin';
 
 import { } from '@auth0/angular-jwt'
@@ -19,38 +19,54 @@ import { routes } from './app.routes';
 import { jwtInterceptor } from 'core/jwt/jwtInterceptor';
 import { AdminState } from 'state/admin/admin.state';
 import { provideReuseStrategy } from 'core/route/app_route_reuse';
+import { JwtOptions, provideJwtOptions } from 'core/jwt/jwt.options';
+import { of } from 'rxjs';
 
 export const Configuration = {
 
-  apiEndpoints:   {
-      login: '/login/',
-      categories: '/api/category/',
-      usersGroup: '/api/group/',
-      users: '/api/user/',
-      question: '/api/question/',
-      cource: '/api/cource/'
+  apiEndpoints: {
+    login: '/login/',
+    categories: '/api/category/',
+    usersGroup: '/api/group/',
+    users: '/api/user/',
+    question: '/api/question/',
+    cource: '/api/cource/'
   },
   apiHost: 'http://api-server:5000'
+}
 
+const primeOptions = {
+  theme: {
+    preset: AppTheme,
+    options: {
+      darkModeSelector: '.dark'
+    }
+  }
+};
+
+
+const jwtOptions: JwtOptions = {
+  blackList:[""],
+  whiteList: [Configuration.apiHost],
+  getTokenFn: ()=>{
+    const store = inject(Store);
+    return store.selectSnapshot((state) => state.ApplicationState.apiToken)
+  },
+  refreshTokenFn: () => {
+    return of("");
+  }
 }
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes), 
-    //provideClientHydration(withEventReplay()),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
     provideAnimationsAsync(),
-        providePrimeNG({
-            theme: {
-                preset: AppTheme,
-                options: {
-                  darkModeSelector: '.dark'
-                }
-        
-            }
-        }),
+    providePrimeNG(primeOptions),
     provideHttpClient(withInterceptors([jwtInterceptor])),
-    provideStore([ApplicationState,AdminState], ...[withNgxsReduxDevtoolsPlugin(), withNgxsStoragePlugin({ keys: '*' })]),
-    provideReuseStrategy(), provideClientHydration(withEventReplay())
+    provideStore([ApplicationState, AdminState], ...[withNgxsReduxDevtoolsPlugin(), withNgxsStoragePlugin({ keys: '*' })]),
+    provideReuseStrategy(), 
+    provideClientHydration(withEventReplay()),
+    provideJwtOptions(jwtOptions)
   ]
 };
--- Client/src/app/core/jwt/jwtInterceptor.ts
+++ Client/src/app/core/jwt/jwtInterceptor.ts
@@ -1,25 +1,81 @@
-import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpInterceptorFn, HttpRequest } from "@angular/common/http";
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpHandlerFn, HttpInterceptor, HttpInterceptorFn, HttpRequest } from "@angular/common/http";
 import { inject } from "@angular/core";
 import { Store } from "@ngxs/store";
 
-import { catchError, Observable, throwError } from "rxjs";
+import { BehaviorSubject, catchError, filter, Observable, switchMap, take, tap, throwError } from "rxjs";
+import { JwtHelperService } from "./jwt-helper.service";
+import { JWT_OPTIONS } from "./jwt.options";
+
+
+
+function isInWhitelist(url: string): boolean {
+  let flag = false;
+  this.options.whiteList?.forEach(f => {
+    if (url.includes(f)) flag = true;
+  });
+  return flag;
+}
+
+function isInBlacklist(url: string): boolean {
+  let flag = false;
+  this.options.blackList?.forEach(f => {
+    if (url.includes(f)) flag = true;
+  });
+  return flag;
+}
+
+var isRefreshing$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+const jwtHelper = inject(JwtHelperService);
+const options = inject(JWT_OPTIONS)
+
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
-    const store = inject(Store);
-  
-    const isAuthOrRoot = req.url.includes('auth');
-    const token = store.selectSnapshot((state) => state.ApplicationState.apiToken)
+  const isAuthOrRoot = req.url.includes('auth');
+  var token = options.getTokenFn()
 
-    if (!isAuthOrRoot && token) {
-      req = req.clone({
-        setHeaders: {
-          Authorization: `Bearer ${token}`
-        }
-      });
-    }
-    return next(req).pipe(
-      catchError((error) => {
-        return throwError(() => error);
+  if (isInWhitelist(req.url) && !isInBlacklist(req.url)) {
+    if (jwtHelper.isTokenExpired(token, 120)) {
+      return refreshToken(req, next);
+    };
+    req = token ? req.clone({ setHeaders: { Authorization: `Bearer ${token}` } }) : req
+  }
+
+  return next(req).pipe(
+    catchError(err => {
+      return throwError(() => err);
+    })
+  );
+
+}
+
+function refreshToken(req: HttpRequest<any>, next: HttpHandlerFn) {
+  if (!isRefreshing$.getValue()) {
+    if (!options.refreshTokenFn) { return throwError(() => new Error('Refresh function catn`t be null')) }
+    return options.refreshTokenFn().pipe(
+      tap(ref => {
+        this.isRefreshing$.next(false);
+      }),
+      switchMap(token => {
+        req = req.clone({
+          setHeaders: { Authorization: `Bearer ${token}` }
+        });
+        return next(req);
+      }),
+      catchError(err => {
+        return throwError(() => err);
       })
     );
   }
+
+  return this.isRefreshing$.pipe(
+    filter((is) => !is),
+    take(1),
+    switchMap(() => {
+      const token = this.tokenGetter(req)
+      req = req.clone({
+        setHeaders: { Authorization: `Bearer ${token}` }
+      });
+      return next(req);
+    })
+  )
+}
